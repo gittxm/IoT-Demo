1a49aebe0d3d404e1c2e55ac3bdea6a49641eccf
IvanRLevyX98 <144261628+IvanRLevyX98@users.noreply.github.com>
2024-02-07 15:03:24 -0600

    Update sub.js

--- sub.js
+++ sub.js
@@ -8,49 +8,32 @@ const credentials = base64.encode(`${username}:${password}`);
 const elasticsearchUrl = 'http://34.28.106.98:9200';
 const sub = mqtt.connect('mqtt://34.28.106.98:9000');
 var index = '';
-const indexDist = "distancia";
-const indexHumo = "humo";
-const indexLuz = "luz";
 
 var timestamp = new Date().toISOString();
-var timestampDist = new Date().toISOString();
-var timestampHumo = new Date().toISOString();
-var timestampLuz = new Date().toISOString();
-
 
 sub.on('connect', () => {
     sub.subscribe('Tema/Datos')
     sub.subscribe('Tema/TempAgua')
+    sub.subscribe('Tema/Distancia')
+    sub.subscribe('Tema/Luz')
 })
 
 sub.on('message', (topic, message) => {
+  index = '';
 
-  if(topic ==='Tema/Datos'){
-    index = '';
-
-    const mensaje = message.toString();
-
+  if(topic ==='Tema/Datos'){;
     const temperaturaMatch = mensaje.match(/Temperatura: ([\d.]+) °C/);
     const humedadMatch = mensaje.match(/Humedad: ([\d.]+) %/);
-    const distanciaMatch = mensaje.match(/Distancia: ([\d.]+) cm/); 
-    const humoMatch = mensaje.match(/Humo: ([\d.]+) humo/);   
-    const luzMatch = mensaje.match(/Luz: ([\d.]+) luz/); 
-
+    const mensaje = message.toString()
+    
     // Verificar si se encontraron coincidencias y extraer los valores
     const temperatura = temperaturaMatch ? parseFloat(temperaturaMatch[1]) : null;
     const humedad = humedadMatch ? parseFloat(humedadMatch[1]) : null;
-    const distancia = distanciaMatch ? parseFloat(distanciaMatch[1]) : null;
-    const humo = humoMatch ? parseFloat(humoMatch[1]) : null;
-    const luz = luzMatch ? parseFloat(luzMatch[1]) : null;
-
+    
     //console.log(' temp:' + temperatura);
     //console.log('humedad' + humedad);
 
     timestamp = new Date().toISOString();
-    timestampDist = new Date().toISOString();
-    timestampHumo = new Date().toISOString();
-    timestampLuz = new Date().toISOString();
-
     console.log(timestamp);
     const nuevoDocumento =
     {
@@ -60,19 +43,55 @@ sub.on('message', (topic, message) => {
     };
 
     axios.post(`${elasticsearchUrl}/${index}/_doc`, nuevoDocumento, {
-        headers: {
-          'Authorization': `Basic ${credentials}`,
-          'Content-Type': 'application/json'
-        }
-      })
-        .then(response => {
-          console.log('Documento insertado con éxito:', response.data);
-        })
-        .catch(error => {
-          console.error('Error al insertar el documento:', error.message);
-        });
-    
+      headers: {
+        'Authorization': `Basic ${credentials}`,
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      console.log('Documento insertado con éxito:', response.data);
+    })
+    .catch(error => {
+      console.error('Error al insertar el documento:', error.message);
+    });
+      
+  }
 
+  if(topic==='Tema/TempAgua'){
+    index = 'termo_agua';
+    const mensaje = message.toString();
+    console.log("mensaje 142"+ mensaje);
+    const temperaturaAguaMatch = mensaje.match(/TemperaturaAgua:\s*(-?\d+(\.\d+)?)/);
+    const temperatura = temperaturaAguaMatch ? parseFloat(temperaturaAguaMatch[1]) : null;
+    console.log(' temperatura agua :' + temperatura);
+    timestamp = new Date().toISOString();
+    console.log(timestamp);
+    const nuevoDocumento1 = 
+    {
+      "Temperatura":temperatura,
+      "@timestamp" : timestamp 
+    };
+  
+    axios.post(`${elasticsearchUrl}/${index}/_doc`, nuevoDocumento1, {
+      headers: {
+        'Authorization': `Basic ${credentials}`,
+        'Content-Type': 'application/json'
+      }
+    }).then(response => {
+      console.log('Documento insertado con éxito:');
+    }) .catch(error => {
+      console.error('Error al insertar el documento:', error.message);
+    });    
+  }
+
+  if(topic==='Tema/Distancia'){
+    index = 'distancia';
+    const mensaje = message.toString();
+    console.log("mensaje 142"+ mensaje);
+    const distanciaMatch = mensaje.match(/Distancia: ([\d.]+) cm/); 
+    const distancia = distanciaMatch ? parseFloat(distanciaMatch[1]) : null;
+    console.log(' distancia :' + distancia);
+    var timestampDist = new Date().toISOString();
     console.log(timestampDist);
     const nuevoDocumentoDist =
     {
@@ -81,19 +100,56 @@ sub.on('message', (topic, message) => {
     };
 
     axios.post(`${elasticsearchUrl}/${indexDist}/_doc`, nuevoDocumentoDist, {
-        headers: {
-          'Authorization': `Basic ${credentials}`,
-          'Content-Type': 'application/json'
-        }
-      })
-        .then(response => {
-          console.log('Documento insertado con éxito:', response.data);
-        })
-        .catch(error => {
-          console.error('Error al insertar el documento:', error.message);
-        });
-    
+      headers: {
+        'Authorization': `Basic ${credentials}`,
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      console.log('Documento insertado con éxito:', response.data);
+    })
+    .catch(error => {
+      console.error('Error al insertar el documento:', error.message);
+    });
+  }
 
+  if(topic==='Tema/Luz'){
+    index = 'luz';
+    const mensaje = message.toString();
+    console.log("mensaje 142"+ mensaje);
+    const luzMatch = mensaje.match(/Luz: ([\d.]+) luz/); 
+    const luz = luzMatch ? parseFloat(luzMatch[1]) : null;
+    console.log(' luz :' + luz);
+    var timestampLuz = new Date().toISOString();
+    console.log(timestampLuz);
+    const nuevoDocumentoLuz =
+    {
+        "Luz" : luz, 
+        "@timestamp" : timestampLuz
+    };
+
+    axios.post(`${elasticsearchUrl}/${indexLuz}/_doc`, nuevoDocumentoLuz, {
+      headers: {
+        'Authorization': `Basic ${credentials}`,
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      console.log('Documento insertado con éxito:', response.data);
+    })
+    .catch(error => {
+      console.error('Error al insertar el documento:', error.message);
+    });
+  }
+
+  if(topic==='Tema/Humo'){
+    index = 'humo';
+    const mensaje = message.toString();
+    console.log("mensaje 142"+ mensaje);
+    const humoMatch = mensaje.match(/Humo: ([\d.]+) humo/);   
+    const humo = humoMatch ? parseFloat(humoMatch[1]) : null;
+    console.log(' luz :' + luz);
+    var timestampHumo = new Date().toISOString();
     console.log(timestampHumo);
     const nuevoDocumentoHumo =
     {
@@ -102,75 +158,16 @@ sub.on('message', (topic, message) => {
     };
 
     axios.post(`${elasticsearchUrl}/${indexHumo}/_doc`, nuevoDocumentoHumo, {
-        headers: {
-          'Authorization': `Basic ${credentials}`,
-          'Content-Type': 'application/json'
-        }
-      })
-        .then(response => {
-          console.log('Documento insertado con éxito:', response.data);
-        })
-        .catch(error => {
-          console.error('Error al insertar el documento:', error.message);
-        });
-    
-
-      console.log(timestampLuz);
-      const nuevoDocumentoLuz =
-      {
-          "Luz" : luz, 
-          "@timestamp" : timestampLuz
-      };
-  
-      axios.post(`${elasticsearchUrl}/${indexLuz}/_doc`, nuevoDocumentoLuz, {
-          headers: {
-            'Authorization': `Basic ${credentials}`,
-            'Content-Type': 'application/json'
-          }
-        })
-          .then(response => {
-            console.log('Documento insertado con éxito:', response.data);
-          })
-          .catch(error => {
-            console.error('Error al insertar el documento:', error.message);
-          });
-        }
-
-        if(topic==='Tema/TempAgua'){
-
-          index = 'termo_agua';
-          const mensaje = message.toString();
-          console.log("mensaje 142"+ mensaje);
-          const temperaturaAguaMatch = mensaje.match(/TemperaturaAgua:\s*(-?\d+(\.\d+)?)/);
-          const temperatura = temperaturaAguaMatch ? parseFloat(temperaturaAguaMatch[1]) : null;
-          console.log(' temperatura agua :' + temperatura);
-          timestamp = new Date().toISOString();
-          console.log(timestamp);
-          const nuevoDocumento1 = 
-          {
-            "Temperatura":temperatura,
-            "@timestamp" : timestamp 
-          };
-    
-       
-        
-    
-        axios.post(`${elasticsearchUrl}/${index}/_doc`, nuevoDocumento1, {
-            headers: {
-              'Authorization': `Basic ${credentials}`,
-              'Content-Type': 'application/json'
-            }
-            }).then(response => {
-              console.log('Documento insertado con éxito:');
-            }) .catch(error => {
-              console.error('Error al insertar el documento:', error.message);
-            });
-        
-    
-    
-    
-        }
-
-
-
-      })
+      headers: {
+        'Authorization': `Basic ${credentials}`,
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      console.log('Documento insertado con éxito:', response.data);
+    })
+    .catch(error => {
+      console.error('Error al insertar el documento:', error.message);
+    });
+  }
+})
